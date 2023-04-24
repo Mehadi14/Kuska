559043516d8e2376fb5d0abb20e216dd520eb379
Mehadi Athani <mehadi.a@fireflink.com>
2023-04-24 13:11:27 +0530

    some files are added

---
+++ src/main/java/com/git/pract/NoSweet/NoSweetApplication.java
@@ -0,0 +1,13 @@
+package com.git.pract.NoSweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/pract/NoSweet/NoSweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.pract.NoSweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
